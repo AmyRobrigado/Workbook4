4ca527a5cf33bdb81fa3779ae25ff789efaf1f68
AmyRobrigado <amyantanod@gmail.com>
2022-10-31 14:40:15 -0400

    Modified file and added new file

---
+++ ObjectScripts/label_maker.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+let myInfo = {
+    myName: "Amy Robrigado",
+    address: "378-12 Hayeward Ave",
+    city: "New Brunswick",
+    state: "New York",
+    zip: "19933",
+}
+
+function printContact(myInfo) {
+    console.log(myInfo);
+}
+
+printContact(myInfo);
+
